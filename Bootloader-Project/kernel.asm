f0fc8f5d0bf9e039df8cc8712e6aad6bae129bbf
thiago <tsa3@cin.ufpe.br>
2019-09-15 14:55:09 -0300

    Menu Finished
    The menu have a link with kernel.

--- Bootloader-Project/kernel.asm
+++ Bootloader-Project/kernel.asm
@@ -1,10 +1,20 @@
-org 0x7e00
+org 0x8600
 jmp 0x0000:start
 
 start:
-xor ax, ax
-mov ds, ax
-mov es, ax
+    xor ax, ax
+    mov ds, ax
+    mov es, ax
+
+    mov ah, 0
+    mov al, 12h
+    int 10h
+
+    mov ah, 0xb
+    mov bh, 0
+    mov bl, 10
+    int 10h
+
 
 done:
 jmp $
--- Bootloader-Project/menu.asm
+++ Bootloader-Project/menu.asm
@@ -2,16 +2,18 @@
 org 0x7E00
 jmp 0x0000:start
 
+;Titulo e Menu
 title db 'Adventure Bit', 13
-start db 'Play', 13
+begin db 'Play', 13
 guide db 'Guide', 13
 
 ;Regras
-guide_title             db '-----Guide-----', 13
-instruction             db 'O jogo consiste em percorrer o menor caminho para fugir do labirinto', 13
+instruction             db 'O jogo consiste em percorrer o menor caminho para fugir labirinto', 13
 instructions_to_play    db 'How to play', 13
-how_play                db 'W - Cima, S - Baixo, A - Esquerda, D - Direita', 13
-start_game              db 'Press SPACE to start!', 13
+up                      db 'W - Up', 13
+down                    db 'S - Down', 13
+left                    db 'A - Left', 13
+right                   db 'D - right', 13
 
 start:
     mov ah, 0
@@ -19,16 +21,17 @@ start:
     int 10h
 
     call telainicial
+
     jmp exit
 
 telainicial:
     ;Colorindo a tela
     mov ah, 0xb
     mov bh, 0
-    mov bl, 0
+    mov bl, 15
     int 10h
 
-    ;Setando o curso
+    ;Setando o cursor
     mov ah, 02h
     mov bh, 00h
     mov dh, 07h
@@ -38,22 +41,18 @@ telainicial:
     ;Printando Título
     mov si, title
     call print_parts
-    call endl
-    mov si, guide_title
-    call print_parts
-    call endl
-    mov si, instruction
-    call print_parts
-    call endl
-    mov si, instructions_to_play
-    call print_parts
-    call endl
-    mov si, how_play
-    call print_parts
-    call endl
 
+    ;Chama o menu
+    call menu
 
-    
+    ;Verifica se foi selecionado as regras
+    cmp cx, 2
+    je rules
+
+    ;Inicia o jogo, vai para o kernel
+    call jogo
+
+    ;Printa a string
     print_parts:
         lodsb
         mov ah, 0xe
@@ -65,7 +64,218 @@ telainicial:
 
         cmp al, 13
         jne print_parts
+        ret
 
+    ;Imprime as regras do jogo
+    rules:
+
+        ;Modo vga
+        mov ah, 0
+        mov al, 12h
+        int 10h
+
+        ;Muda cor da tela
+        mov ah, 0xb
+        mov bh, 0
+        mov bl, 15
+        int 10h
+
+        ;Setando o cursor
+        mov ah, 02h
+        mov bh, 00h
+        mov dh, 02h
+        mov dl, 07h
+        int 10h
+
+        ;Imprime Guide
+        mov ah, 0xf
+        mov si, guide
+        call prints
+        call endl
+
+        ;Imprime Instruções
+        mov ah, 02h
+        mov bh, 00h
+        mov dh, 03h
+        mov dl, 07h
+        int 10h
+        mov si, instruction
+        call prints
+        call endl
+
+        ;Imprime Comandos
+        mov ah, 02h
+        mov bh, 00h
+        mov dh, 06h
+        mov dl, 07h
+        int 10h
+        mov si, instructions_to_play
+        call prints
+        call endl
+
+        ;W
+        mov ah, 02h
+        mov bh, 00h
+        mov dh, 07h
+        mov dl, 07h
+        int 10h
+        mov si, up
+        call prints
+        call endl
+
+        ;S
+        mov ah, 02h
+        mov bh, 00h
+        mov dh, 08h
+        mov dl, 07h
+        int 10h
+        mov si, down
+        call prints
+        call endl
+
+        ;A
+        mov ah, 02h
+        mov bh, 00h
+        mov dh, 09h
+        mov dl, 07h
+        int 10h
+        mov si, left
+        call prints
+        call endl
+
+        ;D
+        mov ah, 02h
+        mov bh, 00h
+        mov dh, 10
+        mov dl, 07h
+        int 10h
+        mov si, right
+        call prints
+        call endl
+
+        wait_return:
+            mov ah, 0
+            int 16h
+
+            cmp al, 13
+            jne wait_return
+
+        call start
+        ret
+
+menu:
+    ;Inicia tela
+    mov ah, 02h
+    mov bh, 00h
+    mov dh, 10h
+    mov dl, 10h
+    int 10h
+
+    ;Imprime onde o local que o cursor se encontra
+	mov ah, 0xe
+	mov al, '>'
+	mov bh, 0
+	mov bl, 0xf
+	int 10h
+
+    ;Imprime Start
+    mov si, begin
+    call prints
+
+    ;Modifica a posição do cursor
+    mov ah, 02h
+    mov bh, 00h
+    mov dh, 12h
+    mov dl, 11h
+    int 10h
+
+    ;Imprime Guide
+    mov si, guide
+    call prints
+
+    ;Coloca a posição do cursor com 1
+    mov cx, 1
+    call change_cursor
+
+    ;Retorna
+    ret
+
+change_cursor:
+    mov ah, 0
+    int 16h
+
+    cmp al, 's'
+    je baixo
+
+    cmp al, 'w'
+    je cima
+
+    cmp al, 13
+    jne change_cursor
+
+    ret
+
+    baixo:
+        cmp cx, 2
+        je cima
+
+        mov ah, 02h
+        mov bh, 00h
+        mov dh, 10h
+        mov dl, 10h
+        int 10h
+
+        mov ah, 0xe
+        mov al, 0
+        mov bh, 0
+        mov bl, 0xf
+        int 10h
+
+        mov ah, 02h
+        mov bh, 00h
+        mov dh, 12h
+        mov dl, 10h
+        int 10h
+
+        mov ah, 0xe
+        mov al, '>'
+        mov bh, 0
+        mov bl, 0xf
+        int 10h
+
+        mov cx, 2
+        jmp change_cursor
+    
+    cima:
+        cmp cx, 1
+        je baixo
+
+        mov ah, 02h
+        mov bh, 00h
+        mov dh, 10h
+        mov dl, 10h
+        int 10h
+        
+        mov ah, 0xe
+        mov al, '>'
+        mov bh, 0
+        mov bl, 0xf
+        int 10h
+
+        mov ah, 02h
+        mov bh, 00h
+        mov dl, 12h
+        mov dh, 10h
+        int 10h
+
+        mov ah, 0xe
+        mov al, 0
+        mov bh, 0
+        mov bl, 0xf
+        int 10h
+
+        mov cx, 1
+        jmp change_cursor
 
 delay:
 	mov bp, 350
@@ -77,7 +287,7 @@ delay:
 	dec dx
 	jnz delay2
 
-ret
+    ret
 
 getchar:
     mov ah, 0x00
@@ -96,18 +306,37 @@ endl:
     call putchar
     ret
 
-prints:                     ; mov si, string
-  .loop:
-    lodsb                   ; bota character em al 
-    cmp al, 0
-    je .endloop
-    call putchar
-    jmp .loop
-  .endloop:
-  ret
+prints:
+    lodsb 
+
+    mov ah, 0xe
+    mov bh, 0
+    int 10h
+
+    cmp al, 13
+    jne prints
+
+    ret
+
+jogo:
+;Setando a posição do disco onde kernel.asm foi armazenado(ES:BX = [0x500:0x0])
+	mov ax,0x860		;0x50<<1 + 0 = 0x500
+	mov es,ax
+	xor bx,bx		;Zerando o offset
+
+;Setando a posição da Ram onde o jogo será lido
+	mov ah, 0x02	;comando de ler setor do disco
+	mov al,8		;quantidade de blocos ocupados por jogo
+	mov dl,0		;drive floppy
+
+;Usaremos as seguintes posições na memoria:
+	mov ch,0		;trilha 0
+	mov cl,7		;setor 2
+	mov dh,0		;cabeca 0
+	int 13h
+	jc jogo	;em caso de erro, tenta de novo
 
 break:	
 	jmp 0x8600 		        ; Pula para a posição carregada
 
 exit:
-    jmp 0x7e00              ; Começa o game {Posição de memória alocada para o Kernel}
