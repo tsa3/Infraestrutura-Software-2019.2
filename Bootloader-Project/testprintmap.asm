f2ff5a0724c4d35620d70224011190adcaf7acce
João Pedro <49729323+jpspm@users.noreply.github.com>
2019-09-16 13:38:21 -0300

    Add files via upload

--- Bootloader-Project/testprintmap.asm
+++ Bootloader-Project/testprintmap.asm
@@ -24,45 +24,47 @@ writePixel:
 
 drawMap:
 		.loop:
-			cmp si, -1
-			je .jmpline
-			cmp si, -2
-			je .return
-			cmp al, 8
-			je .incSi
-			jne .pAgain
-		.paint:
-			call writePixel
-			add al, 1
-			inc cx
-			jmp .loop
-		.pAgain:
-			inc cx
-			add al, 1
-			call writePixel
-			jmp .loop
+			cmp si, -1 ;se chega em -1 pula linha
+			je .jmpline ; chama jump line
+			cmp si, -2 ; se chegar em -2 acaba a matriz
+			je .return ;chama a função de retorno
+			jne .cmppx ;compara o pixel se for diferente de -2 e de -1
+		.cmppx:
+			cmp al,8 ;se chegou em 8
+			jne .paint ;se nao chegou chama a funçao pra pintar
+			je .nxtpx ;se chegou chama a next pixel
+		.nxtpx:
+			inc si ;incrementa si
+			mov al, 0 ;zera o contador de vezes que o pixel foi printado
+			jmp .loop ;volta pra loop
+
 		.jmpline:
-			mov cx, 0
-			inc dx
-			cmp bl, 8
-			je .nextReg
-			jne .backReg
-		.backReg:
-			mov si, di
-			add bl, 1
-			jmp .loop
-		.incSi:
-			xor ax, ax
-			inc si
-			jmp .loop
-		.nextReg:
-			xor bx, bx
-			xor ax, ax
-			inc si
-			mov di, si
-			jmp .loop
+			cmp bl, 8 ;compara se printou a mesma linha 8 vezes
+			je  .nextline ; se sim, vai pra proxima linha da matriz
+			jne .backline ;se nao, volta pro começo da linha
+		.nextline:
+			inc si ;incrementa si pra sair de -1
+			mov di, si ;muda o di pro começo da proxima linha
+			inc dx ; incrementa dx pra pular linha no print
+			mov cx, 0 ;volta cx pro começo pra voltar pro começo da linha pro print
+			mov bl, 0 ;zera os contadores
+			mov al, 0
+			jmp .paint ; chama a funçao de print
+		.backline:
+			mov si,di ; volta o si pro começo da linha da matriz
+			inc dx ;pula a linha do print
+			add bl, 1 ; soma 1 no contador
+			jmp .paint ;chama a função de print
+
+		.paint:
+			call writePixel ;chama a funçao de printar
+			inc cx ;incrementa cx
+			add al, 1 ;soma 1 no contador de repetiçao do pixel
+			jmp .loop ;volta pra loop
 		.return:
-			xor ax, ax
+			mov al, 0 ;zera os contadores
+			mov bl, 0 ;
+
 	ret
 
 start:
