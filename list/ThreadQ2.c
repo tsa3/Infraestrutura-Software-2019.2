79069af4d32738102300c343de9536312680dda9
Arthur Henrique <39110264+thurcst@users.noreply.github.com>
2019-10-06 01:33:50 -0300

    Add files via upload

--- list/ThreadQ2.c
+++ list/ThreadQ2.c
@@ -36,17 +36,17 @@ cada usuário aparece pelo menos uma vez em cada arquivo e somente em um arquivo
 #define MAX_ARQUIVOS "8"
 #define QTD_USERS "4"
 
-int N, A, T;                                                                                            //  A = Arquivos, N = Número de usuários, T = Threads    
-FILE *arquivo_1, *arquivo_2, *arquivo_3, *arquivo_4, *arquivo_5, *arquivo_6, *arquivo_7, *arquivo_8;
-pthread_mutex_t MEU_MUTEX = PTHREAD_MUTEX_INITIALIZER;                                                  // Mutex único
-bool *ALREADY_READ;                                                                                     // Pra saber se o arquivo[i] já foi lido ou não.
+int                 N, A, T;                                                                            //  A = Arquivos, N = Número de usuários, T = Threads    
+FILE                *arquivo_1, *arquivo_2, *arquivo_3, *arquivo_4, *arquivo_5, *arquivo_6, *arquivo_7, *arquivo_8;
+pthread_mutex_t     MEU_MUTEX = PTHREAD_MUTEX_INITIALIZER;                                              // Mutex único
+bool                *ALREADY_READ;                                                                      // Pra saber se o arquivo[i] já foi lido ou não.
 
 typedef struct persona                                                                                  // Estrutura padrão pra cada usuário
 {   
-    char NOME[50];
-    char ID[10];    
-    int ULTIMO_ACESSO;
-    double PONTUACAO;
+    char    NOME[50];
+    char    ID[10];    
+    int     ULTIMO_ACESSO;
+    double  PONTUACAO;
 }persona;
 
 persona *DADOS;
@@ -54,7 +54,7 @@ persona *DADOS;
 void *inc(void *threadid)
 {                                                                                                       // Thread geral de leitura de arquivo
     int i = 0;                                      
-    int j = 0;                                      
+    int j = 1;                                      
     while(1)
     {
         if(ALREADY_READ[j] == FALSE && j <= A)                                                              // Se arquivo não tiver sido lido ainda
@@ -67,48 +67,64 @@ void *inc(void *threadid)
                     fscanf(arquivo_1,"nome: % [^\n]s", DADOS[1].NOME);
                     fscanf(arquivo_1,"id: % [^\n]s", DADOS[1].ID);
                     fscanf(arquivo_1,"ultimo_acesso: % [^\n]s",DADOS[1].ULTIMO_ACESSO);
+                    fscanf(arquivo_1, "pontuacao = % [^\n]", DADOS[1].PONTUACAO);
                     break;
+
                 case 2:
                     fscanf(arquivo_2,"nome: %[^\n]s", DADOS[2].NOME);
                     fscanf(arquivo_2,"id: %[^\n]s", DADOS[2].ID);
                     fscanf(arquivo_2,"ultimo_acesso: % [^\n]s",DADOS[2].ULTIMO_ACESSO);
+                    fscanf(arquivo_2, "pontuacao = % [^\n]", DADOS[2].PONTUACAO);
                     break;
+
                 case 3:
                     fscanf(arquivo_3,"nome: %[^\n]s", DADOS[3].NOME);
                     fscanf(arquivo_3,"id: %[^\n]s", DADOS[3].ID);
                     fscanf(arquivo_3,"ultimo_acesso: % [^\n]s",DADOS[3].ULTIMO_ACESSO);
+                    fscanf(arquivo_3, "pontuacao = % [^\n]", DADOS[3].PONTUACAO);
                     break;
+
                 case 4:
                     fscanf(arquivo_4,"nome: %[^\n]s", DADOS[4].NOME);
                     fscanf(arquivo_4,"id: %[^\n]s", DADOS[4].ID);
                     fscanf(arquivo_4,"ultimo_acesso: % [^\n]s",DADOS[4].ULTIMO_ACESSO);
+                    fscanf(arquivo_4, "pontuacao = % [^\n]", DADOS[4].PONTUACAO);
                     break;
+
                 case 5:
                     fscanf(arquivo_5,"nome: %[^\n]s", DADOS[5].NOME);
                     fscanf(arquivo_5,"id: %[^\n]s", DADOS[5].ID);
                     fscanf(arquivo_5,"ultimo_acesso: % [^\n]s",DADOS[5].ULTIMO_ACESSO);
+                    fscanf(arquivo_5, "pontuacao = % [^\n]", DADOS[5].PONTUACAO);
                     break;
+
                 case 6:
                     fscanf(arquivo_6,"nome: %[^\n]s", DADOS[6].NOME);
                     fscanf(arquivo_6,"id: %[^\n]s", DADOS[6].ID);
                     fscanf(arquivo_6,"ultimo_acesso: % [^\n]s",DADOS[6].ULTIMO_ACESSO);
+                    fscanf(arquivo_6, "pontuacao = % [^\n]", DADOS[6].PONTUACAO);
                     break;
+
                 case 7:
                     fscanf(arquivo_7,"nome: %[^\n]s", DADOS[7].NOME);
                     fscanf(arquivo_7,"id: %[^\n]s", DADOS[7].ID);
                     fscanf(arquivo_7,"ultimo_acesso: % [^\n]s",DADOS[7].ULTIMO_ACESSO);
+                    fscanf(arquivo_7, "pontuacao = % [^\n]", DADOS[7].PONTUACAO);
                     break;
+
                 case 8:
                     fscanf(arquivo_8,"nome: %[^\n]s", DADOS[8].NOME);
                     fscanf(arquivo_8,"id: %[^\n]s", DADOS[8].ID);
                     fscanf(arquivo_8,"ultimo_acesso: % [^\n]s",DADOS[8].ULTIMO_ACESSO);
+                    fscanf(arquivo_8, "pontuacao = % [^\n]", DADOS[8].PONTUACAO);
                     break;
+
                 default:
                     break;
             }
         pthread_mutex_unlock(&MEU_MUTEX);
         }
-        else j++;
+        else if (j<A) j++;
 
     }
     
@@ -144,11 +160,11 @@ int main(){
     gotta_open_them_all(&arquivo_1, &arquivo_2, &arquivo_3, &arquivo_4, &arquivo_5, &arquivo_6, &arquivo_7, &arquivo_8);
     
     // Sessão de vetores
-    pthread_t *VETOR_THREADS;
-    double MEDIA_INAT;
+    pthread_t   *VETOR_THREADS;
+    double      MEDIA_INAT;
     
     // Sessão de contadores
-    int i;
+    int         i;
     
     // Começando os trabalhos
     
