65ee2bb7fcd3c5f2cbeacee73a676d6c0483a301
Arthur Henrique <39110264+thurcst@users.noreply.github.com>
2019-10-08 02:07:50 -0300

    Mudar forma de leitura, concatenar ID com banco

--- list/ThreadQ2.c
+++ list/ThreadQ2.c
@@ -30,6 +30,7 @@ cada usuário aparece pelo menos uma vez em cada arquivo e somente em um arquivo
 #include <pthread.h>
 #include <string.h>
 #include <stdbool.h>
+#include <math.h>
 
 #define TRUE 1
 #define FALSE 0
@@ -37,9 +38,10 @@ cada usuário aparece pelo menos uma vez em cada arquivo e somente em um arquivo
 #define QTD_USERS "4"
 
 int                 N, A, T;                                                                            //  A = Arquivos, N = Número de usuários, T = Threads    
-FILE                *arquivo_1, *arquivo_2, *arquivo_3, *arquivo_4, *arquivo_5, *arquivo_6, *arquivo_7, *arquivo_8;
+FILE                *arquivo;
 pthread_mutex_t     MEU_MUTEX = PTHREAD_MUTEX_INITIALIZER;                                              // Mutex único
 bool                *ALREADY_READ;                                                                      // Pra saber se o arquivo[i] já foi lido ou não.
+char                banco[10] = "banco";
 
 typedef struct persona                                                                                  // Estrutura padrão pra cada usuário
 {   
@@ -59,105 +61,16 @@ void *inc(void *threadid)
     {
         if(ALREADY_READ[j] == FALSE && j <= A)                                                              // Se arquivo não tiver sido lido ainda
         {
-            ALREADY_READ[j] = 1;
-            pthread_mutex_lock(&MEU_MUTEX);
-            switch (j)
-            {
-                case 1:
-                    fscanf(arquivo_1,"nome: % [^\n]s", DADOS[1].NOME);
-                    fscanf(arquivo_1,"id: % [^\n]s", DADOS[1].ID);
-                    fscanf(arquivo_1,"ultimo_acesso: % [^\n]s",DADOS[1].ULTIMO_ACESSO);
-                    fscanf(arquivo_1, "pontuacao = % [^\n]", DADOS[1].PONTUACAO);
-                    break;
+            ALREADY_READ[j] = TRUE;
 
-                case 2:
-                    fscanf(arquivo_2,"nome: %[^\n]s", DADOS[2].NOME);
-                    fscanf(arquivo_2,"id: %[^\n]s", DADOS[2].ID);
-                    fscanf(arquivo_2,"ultimo_acesso: % [^\n]s",DADOS[2].ULTIMO_ACESSO);
-                    fscanf(arquivo_2, "pontuacao = % [^\n]", DADOS[2].PONTUACAO);
-                    break;
-
-                case 3:
-                    fscanf(arquivo_3,"nome: %[^\n]s", DADOS[3].NOME);
-                    fscanf(arquivo_3,"id: %[^\n]s", DADOS[3].ID);
-                    fscanf(arquivo_3,"ultimo_acesso: % [^\n]s",DADOS[3].ULTIMO_ACESSO);
-                    fscanf(arquivo_3, "pontuacao = % [^\n]", DADOS[3].PONTUACAO);
-                    break;
-
-                case 4:
-                    fscanf(arquivo_4,"nome: %[^\n]s", DADOS[4].NOME);
-                    fscanf(arquivo_4,"id: %[^\n]s", DADOS[4].ID);
-                    fscanf(arquivo_4,"ultimo_acesso: % [^\n]s",DADOS[4].ULTIMO_ACESSO);
-                    fscanf(arquivo_4, "pontuacao = % [^\n]", DADOS[4].PONTUACAO);
-                    break;
-
-                case 5:
-                    fscanf(arquivo_5,"nome: %[^\n]s", DADOS[5].NOME);
-                    fscanf(arquivo_5,"id: %[^\n]s", DADOS[5].ID);
-                    fscanf(arquivo_5,"ultimo_acesso: % [^\n]s",DADOS[5].ULTIMO_ACESSO);
-                    fscanf(arquivo_5, "pontuacao = % [^\n]", DADOS[5].PONTUACAO);
-                    break;
-
-                case 6:
-                    fscanf(arquivo_6,"nome: %[^\n]s", DADOS[6].NOME);
-                    fscanf(arquivo_6,"id: %[^\n]s", DADOS[6].ID);
-                    fscanf(arquivo_6,"ultimo_acesso: % [^\n]s",DADOS[6].ULTIMO_ACESSO);
-                    fscanf(arquivo_6, "pontuacao = % [^\n]", DADOS[6].PONTUACAO);
-                    break;
-
-                case 7:
-                    fscanf(arquivo_7,"nome: %[^\n]s", DADOS[7].NOME);
-                    fscanf(arquivo_7,"id: %[^\n]s", DADOS[7].ID);
-                    fscanf(arquivo_7,"ultimo_acesso: % [^\n]s",DADOS[7].ULTIMO_ACESSO);
-                    fscanf(arquivo_7, "pontuacao = % [^\n]", DADOS[7].PONTUACAO);
-                    break;
-
-                case 8:
-                    fscanf(arquivo_8,"nome: %[^\n]s", DADOS[8].NOME);
-                    fscanf(arquivo_8,"id: %[^\n]s", DADOS[8].ID);
-                    fscanf(arquivo_8,"ultimo_acesso: % [^\n]s",DADOS[8].ULTIMO_ACESSO);
-                    fscanf(arquivo_8, "pontuacao = % [^\n]", DADOS[8].PONTUACAO);
-                    break;
-
-                default:
-                    break;
-            }
-        pthread_mutex_unlock(&MEU_MUTEX);
         }
         else if (j<A) j++;
-
     }
     
 }
 
-void gotta_open_them_all(FILE *arquivo_1, FILE *arquivo_2,FILE *arquivo_3, FILE *arquivo_4, FILE *arquivo_5, FILE *arquivo_6, FILE *arquivo_7, FILE *arquivo_8)
-{
-    arquivo_1 = fopen("./files/banco1.txt", "r+");
-    arquivo_2 = fopen("./files/banco2.txt", "r+");
-    arquivo_3 = fopen("./files/banco3.txt", "r+");
-    arquivo_4 = fopen("./files/banco4.txt", "r+");
-    arquivo_5 = fopen("./files/banco5.txt", "r+");
-    arquivo_6 = fopen("./files/banco6.txt", "r+");
-    arquivo_7 = fopen("./files/banco7.txt", "r+");
-    arquivo_8 = fopen("./files/banco8.txt", "r+");
-}
-
-void gotta_close_them_all(FILE *arquivo_1, FILE *arquivo_2,FILE *arquivo_3, FILE *arquivo_4, FILE *arquivo_5, FILE *arquivo_6, FILE *arquivo_7, FILE *arquivo_8)
-{
-    fclose(arquivo_1);
-    fclose(arquivo_2);
-    fclose(arquivo_3);
-    fclose(arquivo_4); 
-    fclose(arquivo_5);
-    fclose(arquivo_6); 
-    fclose(arquivo_7);
-    fclose(arquivo_8); 
-}
 
 int main(){
-    
-    // Sessão de arquivos
-    gotta_open_them_all(&arquivo_1, &arquivo_2, &arquivo_3, &arquivo_4, &arquivo_5, &arquivo_6, &arquivo_7, &arquivo_8);
     
     // Sessão de vetores
     pthread_t   *VETOR_THREADS;
@@ -169,9 +82,8 @@ int main(){
     // Começando os trabalhos
     
     
-    printf("Como numero maximo, colocamos "MAX_ARQUIVOS" arquivos para serem lidos, cada um deles com "QTD_USERS" usuarios distintos, ja que nao foram dadas informacoes sobre isso.\n");
-    printf(" O que te da uma quantidade de 8x4 usuarios para serem lidos.");
-    printf("Utilize essa informacao pra definir a quantidade de arquivos a serem lidos, pra nao passar do numero max.");
+    printf("Colocamos "MAX_ARQUIVOS" arquivos para serem lidos, cada um deles com "QTD_USERS" usuarios distintos, ja que nao foram dadas informacoes sobre isso.\n");
+    printf(" O que te da uma quantidade de 8x4 usuarios para serem lidos.\n");
     printf("Digite a quantidade de Usuarios:");
     scanf("%d", &N);
     printf("\n");
@@ -200,7 +112,6 @@ int main(){
         pthread_create(&VETOR_THREADS[i], NULL, inc, NULL);
     }
 
-    gotta_close_them_all(&arquivo_1, &arquivo_2, &arquivo_3, &arquivo_4, &arquivo_5, &arquivo_6, &arquivo_7, &arquivo_8);
     free(ALREADY_READ);
     free(VETOR_THREADS);
     free(DADOS);
