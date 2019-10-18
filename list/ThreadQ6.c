ba11742d389735f7385d58d72d8cdd48fc9f2570
João Pedro <49729323+jpspm@users.noreply.github.com>
2019-10-18 00:01:12 -0300

    creio q ta finished

--- list/ThreadQ6.c
+++ list/ThreadQ6.c
@@ -7,14 +7,16 @@
 
 typedef struct parameters{
     int id;
-    bool status = false;
+    bool status;
 }parameters;
 
 int buff[BUFFER_SIZE];  /* buffer size = 10; */
 int itens = 0; // number of items in the buffer.
 int first = 0;
-int last = 0; 
-parameters execs[BUFFER_SIZE];
+int last = 0;
+int ids = 0; 
+int consumido=0;
+parameters execs[NUM_ITEMS];
 
 pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
 pthread_cond_t empty = PTHREAD_COND_INITIALIZER;
@@ -27,15 +29,13 @@ int agendarexec();
 
 main() {
     pthread_t consumer_thread; 
-    pthread_t producer_thread; 
     int escolha;
     int resposta;
     int search;
     pthread_create(&consumer_thread,NULL,consumer,NULL);
-    pthread_create(&producer_thread,NULL,producer,NULL);
-    pthread_join(producer_thread,NULL);  
-    pthread_join(consumer_thread,NULL);
-    do{
+    //pthread_join(producer_thread,NULL);  
+    //pthread_join(consumer_thread,NULL);
+    while(escolha != 3){
         printf("O que deseja fazer?\n"); // menu para auxiliar a organizaçao
         printf("1 - Agendar Execucao\n");
         printf("2 - Conferir Status da execucao\n");
@@ -56,36 +56,34 @@ main() {
         else if(escolha == 3){
             break;
         }
-    }while(escolha != 3);
+    }
 }
-int agendaexec(){
-    execs[itens].status = false; //garante que o status vai estar em false inicialmente
-    producer(); //chama a funçao de produtor
-    execs[itens].id = itens;
-    return execs[itens].id; //retorna o id da execuçao
+int agendarexec(){
+    int aux;
+    pthread_t producer_thread; 
+    pthread_create(&producer_thread,NULL,producer,NULL);
+    execs[ids].status = false; //garante que o status vai estar em false inicialmente
+    execs[ids].id = ids;
+    aux = ids;
+    ids++;
+    return execs[aux].id ; //retorna o id da execuçao
 }
 
 void put(int i){
-    pthread_mutex_lock(&mutex);
-    while(itens == BUFFER_SIZE) {
-        pthread_cond_wait(&empty, &mutex);
-    }
-    buff[last] = i;
-    itens++; 
-    last++;
-    if(last==BUFFER_SIZE) { 
-        last = 0;
-    } 
-    if(itens == 1) { 
-        pthread_cond_signal(&fill); 
-    }
-    pthread_mutex_unlock(&mutex); 
+  pthread_mutex_lock(&mutex);
+  while(itens == BUFFER_SIZE) {
+    pthread_cond_wait(&empty, &mutex);
+  }
+  buff[last] = i;
+  itens++; last++;
+  if(last==BUFFER_SIZE) { last = 0; } 
+  if(itens == 1) { 
+    pthread_cond_signal(&fill); }
+  pthread_mutex_unlock(&mutex);
 }
 void *producer() {
   int i = 0;
-  for(i=0;i<NUM_ITEMS; i++) {
     put(i);
-  }
   pthread_exit(NULL);
 }
 
@@ -96,13 +94,14 @@ int get(){
     pthread_cond_wait(&fill, &mutex);
   }
   result = buff[first];
-  itens--; 
-  first++;
-  if(first==BUFFER_SIZE) { 
-      first = 0; }
+  printf("pos %d: ", first);
+  itens--; first++;
+  if(first==BUFFER_SIZE) { first = 0; }
   if(itens == BUFFER_SIZE - 1){
     pthread_cond_signal(&empty);}
   pthread_mutex_unlock(&mutex);
+  execs[consumido].status = true;
+  consumido++;
   return result;
 }
 void *consumer() {
